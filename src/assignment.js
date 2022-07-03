109a97d3778e67a4ab6b2a57082d6ac14abff5f2
Adetayo Akinsanya <akinsanyaadetayo7@gmail.com>
2022-07-03 20:44:11 +0000

    Chore: Implement functions to work as required

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,18 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    // checkk if the input is an arrayOfNumbers
+    if(!Array.isArray(arrayOfNumbers)){
+      throw  new Error('Please pass an array')
+    }
+
+    const sumation = (acc, curr) => {
+      return acc + curr
+    }
+
+   return arrayOfNumbers.reduce(sumation)
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +37,17 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    if(!Array.isArray(arrayOfNumbers)){
+      throw  new Error('Please pass an array')
+    }
+
+  let evenNumbers = arrayOfNumbers.filter(number => {
+  return number % 2 === 0;
+  });
+
+   return evenNumbers.length;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
@@ -47,9 +64,19 @@ function countEvenNumbers(arrayOfNumbers) {
  * @returns Array the converted temperatures in Fahrenheit
  */
 function celsiusToFahrenheit(arrayOfNumbers) {
-    return arrayOfNumbers;
+  if (!Array.isArray(arrayOfNumbers)) {
+    throw new Error('Please pass an array');
+  }
+
+  const convertToF = (celsius) => {
+    const fahrenheit = celsius * 1.8 + 32;
+    return Math.floor(fahrenheit);
+  };
+
+  return arrayOfNumbers.map(convertToF);
 }
-// assignment.celsiusToFahrenheit = celsiusToFahrenheit;
+assignment.celsiusToFahrenheit = celsiusToFahrenheit;
+
 
 
 // ========================
